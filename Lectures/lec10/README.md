d0c0044859a2d17f1ed73a2efccb781a49a4c6dc
Vinicius Prado da Fonseca <vncprado@gmail.com>
2024-01-26 14:19:36 +0000

    Updating lect10 and creating sandbox folder

---
+++ Lectures/lec10/README.md
@@ -0,0 +1,9 @@
+Verilog examples for Lecture 10 - Verilog For Combinational Logic 
+
+To compile:
+
+    $ iverilog -g2012 and_gate_tb.v and_gate.v -o and_gate
+
+To run:
+
+    $ vvp and_gate
--- Lectures/lec10/and_gate.v
+++ Lectures/lec10/and_gate.v
@@ -2,18 +2,18 @@
 module and_gate(output logic y,
                  input logic a, b);
 
-    //or #2 gate1(y, a, b);
+    and #2 gate1(y, a, b);
     //assign y = ~a & b;
-    logic t;
-    assign y = t;
-    always_comb begin
-        case( {a,b} )
-            2'b00: t = 0;
-            2'b01: t = 0;
-            2'b10: t = 0;
-            2'b11: t = 1;
-        endcase
-    end
+    // logic t;
+    // assign y = t;
+    // always_comb begin
+    //     case( {a,b} )
+    //         2'b00: t = 0;
+    //         2'b01: t = 0;
+    //         2'b10: t = 0;
+    //         2'b11: t = 1;
+    //     endcase
+    // end
 endmodule 
 
 
