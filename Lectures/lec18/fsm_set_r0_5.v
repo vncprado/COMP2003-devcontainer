e278fbfcaa30e860373b9dd8bd303d20d7174346
Vinicius Prado da Fonseca <vncprado@gmail.com>
2024-02-28 10:55:12 -0330

    fsm

---
+++ Lectures/lec18/fsm_set_r0_5.v
@@ -0,0 +1,39 @@
+module controller(
+  output as_ctl_t as_ctl,
+  output shift_ctl_t shift_ctl,
+  output logic r1_ld, r0_ld,
+  input clk, reset
+);
+    enum logic [1:0] { S0, S1, S2 } st; // include other states
+
+    task automatic ctrl(as_ctl_t as,
+                        shift_ctl_t sh,
+                        logic r1,
+                        logic r0 );
+                        
+        as_ctl = as; shift_ctl = sh; r1_ld = r1; r0_ld = r0;
+    endtask
+
+    always_comb begin
+        case( st ) 
+            S0: ctrl(AS_A, SH_ONE, 0, 1 ); // R0 <= 1
+            S1: ctrl(AS_A, SH_ONE, 1, 0 ); // R1 <= R0
+            				    // R0 <= 2*R0
+            			            // R0 <= 2*R0 + R1
+
+            S2: ctrl(AS_A, SH_ONE, 0, 0 );
+        endcase
+    end
+
+    always_ff @(posedge clk) begin
+        if ( reset ) st <= S0;
+        else begin
+            case( st )
+                S0: st <= S1;
+                S1: st <= S2;
+                // other states
+                S2: st <= S2;
+            endcase
+        end
+    end
+endmodule
---
+++ Lectures/lec18/tb_fsm_set_r0_5.v
@@ -0,0 +1,42 @@
+`include "seq_gen.v"
+`include "fsm_set_r0_r1.v"
+
+module main;
+    reg16_t seq_out;
+    logic clk, reset;
+    as_ctl_t as_ctl;
+    shift_ctl_t shift_ctl;
+    logic r1_ld, r0_ld;
+
+    task clock_cycle;
+        clk = 0; #5;
+        clk = 1; #5;
+    endtask
+
+    task automatic display_state(string msg);
+        $display("%6s state=%d R0=%4x R1=%4x as=%b sh=%b r0_ld=%b r1_ld=%b reset=%b",
+            msg, CTL.st, DP.r0_v, DP.r1_v, as_ctl, shift_ctl, r0_ld, r1_ld, reset);
+    endtask
+
+    seq_gen_dp DP(seq_out, clk, reset, as_ctl, shift_ctl, r1_ld, r0_ld);
+    controller CTL( as_ctl, shift_ctl, r1_ld, r0_ld, clk, reset);
+
+    initial begin
+        reset = 1;
+        clock_cycle;
+        display_state("reset");
+        reset = 0;
+        clock_cycle;
+        display_state("R0<=1");
+        clock_cycle;
+        display_state("R1<=R0");
+        clock_cycle;
+                    				    // R0 <= 2*R0
+            			            // R0 <= 2*R0 + R1
+        display_state("IDLE");
+        clock_cycle;
+        display_state("IDLE");
+        $finish;
+    end
+
+endmodule
